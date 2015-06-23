9188c2aca063ac005ec648ab25c885ceab3d3d32
ivanmalamen <malamen@ukr.net>
2015-06-23 16:05:27 +0300

    BUG-39417 set falcon recipe path on windows

--- winpkg/src/scripts/InstallApi.psm1
+++ winpkg/src/scripts/InstallApi.psm1
@@ -381,7 +381,7 @@ function Configure(
         ReplaceString "$ENV:FALCON_HOME\conf\startup.properties" "*.falcon.security.authorization.enabled=false" "*.falcon.security.authorization.enabled=true"
 		Out-File -Append -FilePath "$ENV:FALCON_HOME\conf\startup.properties" -Encoding "default" -InputObject "*.falcon.enableTLS=false"
         $recipeDir = Join-Path $ENV:HDP_DATA_DIR "falcon-recipe"
-        Out-File -Append -FilePath "$ENV:FALCON_HOME\conf\startup.properties" -Encoding "default" -InputObject "falcon.recipe.path=`"$recipeDir\`""
+        Out-File -Append -FilePath "$ENV:FALCON_HOME\conf\client.properties" -Encoding "default" -InputObject "falcon.recipe.path=`"$recipeDir\`""
         New-Item -Path $recipeDir -ItemType directory -ErrorAction SilentlyContinue
         GiveFullPermissions $recipeDir "*S-1-1-0" $true
         Write-Log "Falcon configuration finished"
