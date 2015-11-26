12907ee06e4565e9a2ac9b6cbf13ae46befb8ece
Venkat Ranganathan <venkat@hortonworks.com>
2015-11-26 09:27:34 -0800

    BUG-48855: Feed Submission failed with Error 'Datasource entity vertex must exist'

--- common/src/main/java/org/apache/falcon/metadata/EntityRelationshipGraphBuilder.java
+++ common/src/main/java/org/apache/falcon/metadata/EntityRelationshipGraphBuilder.java
@@ -20,6 +20,7 @@ package org.apache.falcon.metadata;
 
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
+import org.apache.falcon.entity.FeedHelper;
 import org.apache.falcon.entity.ProcessHelper;
 import org.apache.falcon.entity.v0.Entity;
 import org.apache.falcon.entity.v0.EntityType;
@@ -33,7 +34,6 @@ import org.apache.falcon.entity.v0.process.Output;
 import org.apache.falcon.entity.v0.process.Outputs;
 import org.apache.falcon.entity.v0.process.Process;
 import org.apache.falcon.entity.v0.process.Workflow;
-import org.apache.falcon.metadata.util.MetadataUtil;
 import org.apache.falcon.workflow.WorkflowExecutionArgs;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -98,8 +98,12 @@ public class EntityRelationshipGraphBuilder extends RelationshipGraphBuilder {
             }
         }
 
-        addRelationToDatasource(feedVertex, MetadataUtil.getImportDatasourceName(feed),
-                RelationshipLabel.DATASOURCE_IMPORT_EDGE);
+        for (org.apache.falcon.entity.v0.feed.Cluster feedCluster : feed.getClusters().getClusters()) {
+            if (FeedHelper.isImportEnabled(feedCluster)) {
+                addRelationToDatasource(feedVertex, FeedHelper.getImportDatasourceName(feedCluster),
+                        RelationshipLabel.DATASOURCE_IMPORT_EDGE);
+            }
+        }
     }
 
     public void addDatasourceEntity(Datasource dsEntity) {
@@ -197,11 +201,13 @@ public class EntityRelationshipGraphBuilder extends RelationshipGraphBuilder {
     }
 
     public void addRelationToDatasource(Vertex fromVertex, String datasourceName, RelationshipLabel edgeLabel) {
-        Vertex dataSourceVertex = findVertex(datasourceName, RelationshipType.DATASOURCE_ENTITY);
-        if (dataSourceVertex == null) {
-            return;
+        Vertex datasourceVertex = findVertex(datasourceName, RelationshipType.DATASOURCE_ENTITY);
+        if (datasourceVertex == null) { // datasource must exist before adding other entities
+            LOG.error("Illegal State: Datasource entity vertex must exist for {}", datasourceName);
+            throw new IllegalStateException("Datasource entity vertex must exist: " + datasourceName);
         }
-        addEdge(fromVertex, dataSourceVertex, edgeLabel.getName());
+
+        addEdge(fromVertex, datasourceVertex, edgeLabel.getName());
     }
 
     public void addInputFeeds(Inputs inputs, Vertex processVertex) {
