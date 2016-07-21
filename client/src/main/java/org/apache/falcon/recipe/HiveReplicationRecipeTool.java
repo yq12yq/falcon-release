b6744ea7f4950e885f012f47277bb3a4e341a9fe
sowmyaramesh <sowmyaramesh@users.noreply.github.com>
2016-07-20 17:15:58 -0700

    FALCON-2072 BUG-78813 Hive2 URLs in Falcon should allow additional configuration elements in the URL (#421)
    (cherry picked from commit d7b12cb5f5ccd548779a994bad31197e9983b755)
    
    Change-Id: I08021a65df48849237ce79fbe2bc6436f7734898

--- client/src/main/java/org/apache/falcon/recipe/HiveReplicationRecipeTool.java
+++ client/src/main/java/org/apache/falcon/recipe/HiveReplicationRecipeTool.java
@@ -36,6 +36,7 @@ import java.util.Properties;
  */
 public class HiveReplicationRecipeTool implements Recipe {
     private static final String ALL_TABLES = "*";
+    private static final String NOT_APPLICABLE = "NA";
 
     @Override
     public void validate(final Properties recipeProperties) throws Exception {
@@ -128,6 +129,15 @@ public class HiveReplicationRecipeTool implements Recipe {
         if (StringUtils.isNotEmpty(recipeProperties.getProperty(RecipeToolOptions.RECIPE_NN_PRINCIPAL.getName()))) {
             additionalProperties.put(HiveReplicationRecipeToolOptions.CLUSTER_FOR_JOB_NN_KERBEROS_PRINCIPAL.getName(),
                     recipeProperties.getProperty(RecipeToolOptions.RECIPE_NN_PRINCIPAL.getName()));
+        }
+
+        if (StringUtils.isBlank(
+                recipeProperties.getProperty(HiveReplicationRecipeToolOptions.REPLICATION_SOURCE_HS2_EXTRA_OPTS.getName()))) {
+            additionalProperties.put(HiveReplicationRecipeToolOptions.REPLICATION_SOURCE_HS2_EXTRA_OPTS.getName(), NOT_APPLICABLE);
+        }
+        if (StringUtils.isBlank(
+                recipeProperties.getProperty(HiveReplicationRecipeToolOptions.REPLICATION_TARGET_HS2_URI_EXTRA_OPTS.getName()))) {
+            additionalProperties.put(HiveReplicationRecipeToolOptions.REPLICATION_TARGET_HS2_URI_EXTRA_OPTS.getName(), NOT_APPLICABLE);
         }
         return additionalProperties;
     }
